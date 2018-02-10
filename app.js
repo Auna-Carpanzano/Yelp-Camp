ba2797fd41d8364b209934072aba90f6919b6b23
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-10 16:45:35 -0700

    Add newCampground var that stores object for name: name and image: image

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get("/campgrounds", function(req, res) {
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
+  var newCampground = {name: name, image: image};
 });
 
 app.get("/campgrounds/new", function(req, res) {

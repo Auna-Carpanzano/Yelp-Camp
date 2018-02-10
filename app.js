32acff776c54310be25a0b685db9f1028969f5b3
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-10 15:17:46 -0700

    Render the landing page when a GET request is made to "/"

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+  res.render("landing");
+});
+
 app.listen(3000, function() {
   console.log("The YelpCamp Server Has Started");
 });

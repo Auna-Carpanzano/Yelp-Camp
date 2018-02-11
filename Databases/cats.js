0314a71e36078068268c2367326d7653b655982c
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 12:24:02 -0700

    Find every Cat in the database, add err and cats as function parameters

--- Databases/cats.js
+++ Databases/cats.js
@@ -24,3 +24,13 @@ var Cat = mongoose.model("Cat", catSchema);
 //    console.log(cat);
 //  }
 //});
+
+Cat.find({}, function(err, cats) {
+  if (err) {
+    console.log("OH NO, ERROR");
+    console.log(err);
+  } else {
+    console.log("ALL THE CATS...");
+    console.log(cats);
+  }
+});

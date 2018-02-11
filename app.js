56d373372324558e7ea4f8f12ac150d1f0d83604
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 13:05:29 -0700

    Comment out old Campground.create (the hard-coded Granite Hill campground)

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-  {
-    name:"Granite Hill",
-    image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1053&q=80"
-  }, function (err, campground){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("NEWLY CREATE CAMPGROUND");
-        console.log(campground);
-      }
-    });
+//Campground.create(
+//  {
+//    name:"Granite Hill",
+//    image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1053&q=80"
+//  }, function (err, campground){
+//      if (err) {
+//        console.log(err);
+//      } else {
+//        console.log("NEWLY CREATE CAMPGROUND");
+//        console.log(campground);
+//      }
+//    });
 
 app.get("/", function(req, res) {
   res.render("landing");

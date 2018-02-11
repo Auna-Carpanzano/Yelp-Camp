3a2dc32787fd0253a99418097d9e7758efa746df
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-11 12:47:09 -0700

    Add Campground.create with pattern and function that takes parameters of err and campground

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+Campground.create(
+  {
+    name:"Salmon Creek",
+    image:"https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-0.3.5&s=e0421b3f8e3bc5054feeffb2eb317fe3&auto=format&fit=crop&w=1050&q=80"
+  }, function (err, campground){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("NEWLY CREATE CAMPGROUND");
+        console.log(campground);
+      }
+    })
+
 var campgrounds = [
   {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-0.3.5&s=e0421b3f8e3bc5054feeffb2eb317fe3&auto=format&fit=crop&w=1050&q=80"},
   {name:"Granite Hill", image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1053&q=80"},

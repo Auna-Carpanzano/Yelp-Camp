561a2285a9347de8c7e39ed8799c6ea3f4e648ba
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-12 14:47:07 -0700

    Remove Campground.create

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//Campground.create(
-//  {
-//    name:"Granite Hill",
-//    image:"https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&s=73115e54fa3d099fcb2d92ccf12eee41&auto=format&fit=crop&w=1053&q=80",
-//    description: "This is a huge granite hill. No bathrooms, no water, beautiful granite."
-//  }, function (err, campground){
-//      if (err) {
-//        console.log(err);
-//      } else {
-//        console.log("NEWLY CREATE CAMPGROUND");
-//        console.log(campground);
-//      }
-//    });
-
 app.get("/", function(req, res) {
   res.render("landing");
 });

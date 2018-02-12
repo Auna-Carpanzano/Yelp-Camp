b8c1da56055799948f928d3584ba8db94bd9732f
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-12 13:11:29 -0700

    Find one user with specific email, chain .populate "post", add exec to execute callback function

--- Associations/references.js
+++ Associations/references.js
@@ -26,22 +26,30 @@ var User = mongoose.model("User", userSchema);
 //  name: "Bob Belcher"
 //});
 
-Post.create ({
-  title: "How to cook the best burger part 2",
-  content: "Blah blah blah"
-}, function(err, post) {
-  User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      foundUser.post.push(post._id);
-      foundUser.save(function(err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(data);
-        }
-      });
-    }
-  });
+//Post.create ({
+//  title: "How to cook the best burger part 3",
+//  content: "aergeagaergh"
+//}, function(err, post) {
+//  User.findOne({email: "bob@gmail.com"}, function(err, foundUser) {
+//    if (err) {
+//      console.log(err);
+//    } else {
+//      foundUser.post.push(post._id);
+//      foundUser.save(function(err, data) {
+//        if (err) {
+//          console.log(err);
+//        } else {
+//          console.log(data);
+//        }
+//      });
+//    }
+//  });
+//});
+
+User.findOne({email: "bob@gmail.com"}).populate("post").exec(function(err, user) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(user);
+  }
 });

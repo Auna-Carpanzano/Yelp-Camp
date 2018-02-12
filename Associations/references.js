025b250e5bfa96f6d687544073f471264bd3d2f5
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-12 13:18:17 -0700

    Remove postSchema and Post var to move to post.js file

--- Associations/references.js
+++ Associations/references.js
@@ -1,13 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_2");
 
-var postSchema = new mongoose.Schema ({
-  title: String,
-  content: String
-});
-
-var Post = mongoose.model("Post", postSchema);
-
 var userSchema = new mongoose.Schema ({
   email: String,
   name: String,

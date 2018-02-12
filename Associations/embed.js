d309a00601747c4153cb61c5f90f9e340d961dce
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-12 12:22:41 -0700

    Add post to userSchema for data association, move Post var and postSchema var above userSchema

--- Associations/embed.js
+++ Associations/embed.js
@@ -1,19 +1,20 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
-var userSchema = new mongoose.Schema ({
-  email: String,
-  name: String
-});
-
-var User = mongoose.model("User", userSchema);
-
 var postSchema = new mongoose.Schema ({
   title: String,
   content: String
 });
 
 var Post = mongoose.model("Post", postSchema);
+
+var userSchema = new mongoose.Schema ({
+  email: String,
+  name: String,
+  post: [postSchema]
+});
+
+var User = mongoose.model("User", userSchema);
 
 //var newUser = new User ({
 //  email: "charlie@brown.edu",

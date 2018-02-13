9c2e583db5bfcd0ab99b1a27bff6ab934506d361
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 16:19:35 -0700

    Add checkCommentOwnership function as middleware for update route

--- routes/comments.js
+++ routes/comments.js
@@ -49,7 +49,7 @@ router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 });
 
 // UPDATE COMMENTS
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
     if (err) {
       res.redirect("back");

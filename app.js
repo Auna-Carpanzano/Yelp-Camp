fc7d30a1ef568e966bd85b95633f56bf9b19bb40
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 11:28:05 -0700

    Remove comment routes to move into comments.js file

--- app.js
+++ app.js
@@ -36,35 +36,6 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
-  Campground.findById(req.params.id, function(err, campground) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("comments/new", {campground: campground});
-    }
-  });
-});
-
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
-  Campground.findById(req.params.id, function(err, campground) {
-    if (err) {
-      console.log(err);
-      res.redirect("/campgrounds");
-    } else {
-      Comment.create(req.body.comment, function(err, comment) {
-        if (err) {
-          console.log(err);
-        } else {
-          campground.comments.push(comment);
-          campground.save();
-          res.redirect("/campgrounds/" + campground._id);
-        }
-      });
-    }
-  });
-});
-
 // AUTH ROUTES
 
 // SHOW THE REGISTER FORM

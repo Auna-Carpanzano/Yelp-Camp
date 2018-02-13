86a68dca85b056cab2ade7b98aad4514609a9b67
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 11:25:51 -0700

    Remove campground routes to move into campgrounds.js file

--- app.js
+++ app.js
@@ -36,48 +36,6 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
-// INDEX - show all campgrounds
-app.get("/campgrounds", function(req, res) {
-  Campground.find({}, function(err, allCampgrounds){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
-    }
-  });
-});
-
-// CREATE - add new campground to DB
-app.post("/campgrounds", function(req, res) {
-  var name = req.body.name;
-  var image = req.body.image;
-  var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc};
-  Campground.create(newCampground, function(err, newlyCreated) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/campgrounds");
-    }
-  });
-});
-
-// NEW - show form to create new campground
-app.get("/campgrounds/new", function(req, res) {
-  res.render("campgrounds/new");
-});
-
-// SHOW - shows more info about one campground
-app.get("/campgrounds/:id", function (req, res) {
-  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("campgrounds/show", {campground: foundCampground});
-    }
-  });
-});
-
 app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {

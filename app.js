b14bd118a5b0f90aba0db11ace986227cf368c05
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 13:21:26 -0700

    Allow app to use methodOverride

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({

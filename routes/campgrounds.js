46266216eaa44f63760161fb13cd5af43c4bd6ec
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 12:48:22 -0700

    Add isLoggedIn to new and create route as middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
@@ -29,7 +29,7 @@ router.post("/", function(req, res) {
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
 

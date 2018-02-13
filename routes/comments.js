e7f1b82c08d5abb1162725f499e360e4162bb412
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 11:42:01 -0700

    Require express, create new instance of express Router, export router

--- routes/comments.js
+++ routes/comments.js
@@ -1,4 +1,7 @@
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
+var express = require("express");
+var router = express.Router();
+
+router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
@@ -8,7 +11,7 @@ app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   });
 });
 
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
+router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
@@ -26,3 +29,5 @@ app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     }
   });
 });
+
+module.exports = router;

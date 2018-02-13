4d533f39d807b34c5d0e61e05279443aadfbad2b
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-12 20:05:53 -0700

    Create middleware with isLoggedIn function that checks if user is logged in, returns next() or redirects to "/"

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -75,6 +75,13 @@ app.get("/logout", function(req, res) {
   res.redirect("/");
 });
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+};
+
 app.listen(3000, function() {
   console.log("Server started...")
 });

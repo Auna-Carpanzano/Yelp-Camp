b2a09e7909a181357d5ee834d20faa860987068d
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 12:03:22 -0700

    Change route paths to account for appending "/campgrounds/:id/comments" in app.js file

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
 
-router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);
@@ -13,7 +13,7 @@ router.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
   });
 });
 
-router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground) {
     if (err) {
       console.log(err);

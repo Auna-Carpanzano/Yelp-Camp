941ee0befff17e263195566e3372343fb96812de
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-13 17:18:23 -0700

    Incorporate flash to assign error to value

--- middleware/index.js
+++ middleware/index.js
@@ -39,9 +39,10 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
 }
 
 middlewareObj.isLoggedIn = function (req, res, next) {
-  if (req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     return next();
   }
+  req.flash("error", "Please login first");
   res.redirect("/login");
 };
 
